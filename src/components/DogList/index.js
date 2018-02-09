91f1611c00592f53ddaa1c351a75d49ad332356b
ccalbaugh <calbaugh@qca-sdc.com>
2018-02-09 14:05:27 -0600

    feat: define DogList and return  for passing teset

---
+++ src/components/DogList/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const DogList = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default DogList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './components/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
