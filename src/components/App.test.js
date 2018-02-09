defdc684df30a65eb095bde47589fa21add5e7e0
Caden Albaugh <cadenalbaugh@gmail.com>
2018-02-09 15:00:37 -0600

    test: build test intialization functionality

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,8 +3,42 @@ import { shallow } from 'enzyme';
 import { expect } from 'code';
 import sinon from 'sinon';
 import App from './App';
+import * as dogServices from '../services/fetchDogs';
 
 describe('Given `App`', () => {
+
+  let sandbox,
+  fetchDogsStub
+
+  beforeEach(() => {
+
+    sandbox = sinon.createSandbox()
+
+    fetchDogsStub = sandbox.stub(dogServices, 'fetchDogs').resolves(['one', 'two'])
+
+  })
+
+  afterEach(() => {
+
+    sandbox.restore()
+
+  })
+
+  const requiredProps = (overrides = {}) => {
+
+    return {
+      fetchDogs: fetchDogsStub,
+      ...overrides
+    }
+
+  } 
+
+  const renderComponent = (props = requiredProps()) => {
+
+    return shallow(<App {...props} />)
+
+  }
+
 
   it('should contain a `DogList` component', () => {
 
@@ -13,5 +47,7 @@ describe('Given `App`', () => {
     expect(component.find('DogList').exists()).to.be.true();
 
   })
+
+  it('should contain no `dogBreeds`')
 
 })
