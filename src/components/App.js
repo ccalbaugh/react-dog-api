fafb6ca7edf3ab6a2eaa4c3de06e3f93caeabd35
ccalbaugh <calbaugh@qca-sdc.com>
2018-02-09 13:47:20 -0600

    chore: import DogList to fix error

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import DogList from './DogList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <DogList />
       </div>
     );
   }
