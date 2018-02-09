6c8e8099006883346ef4355e7bbbc061b814e54e
ccalbaugh <calbaugh@qca-sdc.com>
2018-02-09 13:39:46 -0600

    test: ensure App contains DogList

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import App from './App';
 
+describe('Given `App`', () => {
 
+  it('should contain a `DogList` component', () => {
+
+    const component = shallow(<App />);
+
+    expect(component.find('DogList').exists()).to.be.true();
+
+  })
+
+})
