c66e7589e8baaacc881b894394c94a4f380b4944
Caden Albaugh <cadenalbaugh@gmail.com>
2018-02-09 16:45:09 -0600

    chore: addpropTypes and explicit prop in DogList

--- src/components/DogList/index.js
+++ src/components/DogList/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const DogList = () => {
+const DogList = (dogs) => {
     return (
         <div className="dog-list"></div>
     );
 };
+
+DogList.propTypes = {
+    dogs: PropTypes.array.isRequired
+}
 
 export default DogList;
