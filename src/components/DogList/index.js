1644937c4ca522aaf83f5d4cb145d2a3e2301fef
Caden Albaugh <cadenalbaugh@gmail.com>
2018-02-10 09:31:53 -0600

    feat: return a list of dog data

--- src/components/DogList/index.js
+++ src/components/DogList/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const DogList = (dogs) => {
+const DogList = ({ dogs }) => {
+    console.log(dogs)
     return (
         <ul className="dog-list">
-            
+            {
+                dogs.length === 0 ? (
+                    <p>Loading...</p>
+                ) : (
+                    dogs.map((dog) => {
+                      return  <li key={`${dog.breed}-key`} className="dog-list-item">Breed: {dog.breed}</li>
+                    })
+                )
+            }
         </ul>
     );
 };
